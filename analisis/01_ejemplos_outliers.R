0520f82c915ae5ff18a0a5835e5d173b27b4dc5b
wparedes <wparedesgt@gmail.com>
2021-08-12 14:16:51 -0600

    outliers

---
+++ analisis/01_ejemplos_outliers.R
@@ -0,0 +1,21 @@
+#outliers ejemplos 
+
+conjunto01 <- c(150,160,130,150,120)
+conjunto02 <- c(150,160,130,150,120,180)
+conjunto03 <- c(150,160,130,150,120,350)
+conjunto04 <- c(150,160,130,150,120,300,320,340,350)
+
+
+mean(conjunto01)
+mean(conjunto02)
+mean(conjunto03)
+mean(conjunto04)
+
+
+
+
+median(conjunto01)
+median(conjunto02)
+median(conjunto03)
+median(conjunto04)
+
---
+++ analisis/02_BrainBodyWeight.R
@@ -0,0 +1,23 @@
+#Peso de cuerpo y cerebro de 28 especies
+
+library(MASS)
+library(tidyverse)
+
+#cargando los datos
+
+data("Animals")
+View(Animals)
+
+
+#Plot de Regresion
+
+plotbb <- function(bbdat) {
+  d.name <- deparse(substitute(bbdat))
+  plot(log(brain) ~ log(body), data = bbdat, main = d.name)
+  abline(lm(log(brain) ~ log(body), data = bbdat))
+  abline(MASS::rlm(log(brain) ~ log(body), data = bbdat), col = 2)
+  legend("bottomright", leg = c("lm", "rlm"), col=1:2, lwd = 1, inset = 1/20)
+  
+}
+
+plotbb(Animals)

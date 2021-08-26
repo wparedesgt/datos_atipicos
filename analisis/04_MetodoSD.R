ab042b305cf5e28c10dbfe97ebb38d3fe126c196
wparedesgt <wparedesgt@gmail.com>
2021-08-26 14:41:25 -0600

    Metodo SD

---
+++ analisis/04_MetodoSD.R
@@ -0,0 +1,12 @@
+#Metodo SD
+
+Ejemplo05 <- c(3.2, 3.4, 3.7, 3.8, 3.9, 4, 4, 4.1, 4.2, 4.7, 4.8, 14, 15)
+Ejemplo06 <- c(3.2, 3.4, 3.7, 3.8, 3.9, 4, 4, 4.1, 4.2, 4.7, 4.8, 14, 15, 40)
+
+mean(Ejemplo05)
+median(Ejemplo05)
+sd(Ejemplo05)
+
+mean(Ejemplo06)
+median(Ejemplo06)
+sd(Ejemplo06)

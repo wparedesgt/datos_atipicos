b63adb08a33be0892d2e03042ac7c6727b0a2c4b
wparedesgt <wparedesgt@gmail.com>
2021-08-26 11:24:36 -0600

    cambios

--- analisis/00_ejemplo.R
+++ analisis/00_ejemplo.R
@@ -3,5 +3,5 @@
 library(usethis)
 
 usethis::use_git_config(user.name = "wparedesgt", user.email = "wparedesgt@gmail.com")
-credentials::set_github_pat("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")
+credentials::set_github_pat("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")
 usethis::git_sitrep()
--- analisis/03_Estimadores.R
+++ analisis/03_Estimadores.R
@@ -3,6 +3,7 @@
 
 ejemplo01 <- c(1:5)
 ejemplo02 <- c(1:4, 50)
+n <- 5
 
 #Promedio
 mean(ejemplo01)
@@ -12,4 +13,42 @@ mean(ejemplo02)
 
 median(ejemplo01)
 median(ejemplo02)
+
+#Rango y Rango interquartilico
+
+#Rango
+
+diff(range(ejemplo01))
+diff(range(ejemplo02))
+
+#Interquartil
+
+IQR(ejemplo01)
+IQR(ejemplo02)
+
+#Desviacion estandard
+
+factor <- sqrt((n-1)/n) #factor de correcion de prueba en la practica se utiliza el no sezgado sd()
+
+factor*sd(ejemplo01)
+factor*sd(ejemplo02)
+
+#MAD
+
+mad(ejemplo01, constant = 1) #Cuando no se multiplica por el factor 
+mad(ejemplo02, constant = 1)
+
+#Coeficiente de Asimetria
+
+library(e1071)
+
+skewness(ejemplo01)
+skewness(ejemplo02)
+
+#meadcouple
+
+library(mrfDepth)
+
+medcouple(ejemplo01)
+medcouple(ejemplo02)
 

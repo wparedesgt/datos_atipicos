aa4465eab38bc67d44e4738d9e472064f899dac1
wparedesgt <wparedesgt@gmail.com>
2021-09-02 07:06:13 -0600

    modificacion token

--- analisis/00_ejemplo.R
+++ analisis/00_ejemplo.R
@@ -3,5 +3,5 @@
 library(usethis)
 
 usethis::use_git_config(user.name = "wparedesgt", user.email = "wparedesgt@gmail.com")
-credentials::set_github_pat("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")
+credentials::set_github_pat("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")
 usethis::git_sitrep()
